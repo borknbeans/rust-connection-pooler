c6b8c19bb8aadbbf5e1733e63f350c177186b9f2
Ethan Bork <ebork11@gmail.com>
2025-11-25 19:04:40 -0800

    progress towards phase 2, very broken currently

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tokio::net::{TcpListener, TcpStream};
 
+mod postgres;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:6432").await?;
@@ -16,26 +18,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn handle_client(client_stream: TcpStream) -> Result<(), Box<dyn std::error::Error>>{
-    let backend_stream = TcpStream::connect("127.0.0.1:5432").await?;
+    let mut pg_connection = postgres::connect_to_postgres().await?;
 
-    let (mut client_reader, mut client_writer) = client_stream.into_split();
-    let (mut backend_reader, mut backend_writer) = backend_stream.into_split();
+    let (mut client_reader, mut client_writer) = tokio::io::split(client_stream);
+    let (mut backend_reader, mut backend_writer) = tokio::io::split(pg_connection.stream);
 
     let client_to_backend = tokio::io::copy(&mut client_reader, &mut backend_writer);
     let backend_to_client = tokio::io::copy(&mut backend_reader, &mut client_writer);
 
-    tokio::select! {
-        result = client_to_backend => {
-            if let Err(e) = result {
-                eprintln!("Error forwarding client->backend: {}", e);
-            }
-        }
-        result = backend_to_client => {
-            if let Err(e) = result {
-                eprintln!("Error forwarding backend->client: {}", e);
-            }
-        }
+    let (client_result, backend_result) = tokio::join!(client_to_backend, backend_to_client);
+
+    if let Err(e) = client_result {
+        eprintln!("Error forwarding client->backend: {}", e);
     }
-    
+
+    if let Err(e) = backend_result {
+        eprintln!("Error forwarding backend->client: {}", e);
+        pg_connection.state = postgres::ConnectionState::Broken;
+    }
+
+    pg_connection.stream = backend_reader.unsplit(backend_writer);
+    postgres::release_connection(pg_connection).await;
+
     Ok(())
 }
